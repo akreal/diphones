7942947b3452ea6ec99706006f0807a0c51524e5
Pavel Denisov <pavel.a.denisov@gmail.com>
2017-04-24 17:09:08 +0200

    Add corpora files and sort phrase lists alphabetically

--- scripts/ngrams.py
+++ scripts/ngrams.py
@@ -5,7 +5,7 @@ n = 2
 
 ngrams = dict()
 
-for transcription_filename in glob.glob('data/LibriSpeech/*/*/*/*.trans.txt'):
+for transcription_filename in glob.glob('data/corpora/*/*.txt'):
     with open(transcription_filename) as transcription_file:
         for string in transcription_file:
             for ngram in [' '.join(x) for x in zip(*[string.split()[i+1:] for i in range(n)])]:
@@ -14,5 +14,5 @@ for transcription_filename in glob.glob('data/LibriSpeech/*/*/*/*.trans.txt'):
                 else:
                     ngrams[ngram] = 1
 
-for ngram in sorted(ngrams.items(), key=operator.itemgetter(1), reverse=True):
+for ngram in sorted(sorted(ngrams.items(), key=operator.itemgetter(0)), key=operator.itemgetter(1), reverse=True):
     print(str(ngram[1]) + ':' + ngram[0])
