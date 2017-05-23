27efe9f9825cd4a9a9cc7b27c68a4f5056219611
Pavel Denisov <pavel.a.denisov@gmail.com>
2017-05-23 13:29:30 +0200

    Add timestamp information to TED-LIUM transcripts

--- scripts/import_tedlium.py
+++ scripts/import_tedlium.py
@@ -9,4 +9,4 @@ for input_filename in glob.glob(sys.argv[1] + '/*/stm/*.stm'):
     with open(input_filename) as input_file, open(output_filename, 'w') as output_file:
         for string in input_file:
             parts = string.split()
-            output_file.write(' '.join([parts[0]] + [x.upper() for x in parts[6:]]) + '\n')
+            output_file.write(' '.join([parts[0] + '@%s:%s' % (parts[3], parts[4])] + [x.upper() for x in parts[6:]]) + '\n')
