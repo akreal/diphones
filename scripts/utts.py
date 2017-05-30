5c22ad97fc12014d7e29444f76a27a64ebe5acc0
Pavel Denisov <pavel.a.denisov@gmail.com>
2017-05-30 23:39:10 +0200

    Use only aligned utterances for words

--- scripts/utts.py
+++ scripts/utts.py
@@ -1,5 +1,11 @@
 import glob
 
+aligned = set()
+
+with open('data/aligned.txt') as aligned_file:
+    for string in aligned_file:
+        aligned.add(string.strip())
+
 words = set()
 
 with open('data/words.txt') as words_file:
@@ -11,8 +17,11 @@ transcripts = dict()
 for transcription_filename in glob.glob('data/corpora/*/*.txt'):
     with open(transcription_filename) as transcription_file:
         for string in transcription_file:
+            string = string.strip()
             si = string.index(' ')
-            transcripts[string[:si]] = ' _SIL_ ' + string[si + 1:] + ' _SIL_ '
+            utt = string[:si].replace('@', '-').replace(':', '_')
+            if utt in aligned:
+                transcripts[utt] = ' _SIL_ ' + string[si + 1:] + ' _SIL_ '
 
 
 with open('data/utts.txt', 'w') as utts_file:
